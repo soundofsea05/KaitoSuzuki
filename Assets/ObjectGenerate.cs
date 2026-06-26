64bdc2028180f9b0dea6ab2b3de1974b830d799c
mimiduqu <145083878+soundofsea05@users.noreply.github.com>
2026-06-26 19:01:47 +0900

    update ObjectGenerate.cs

--- Assets/ObjectGenerate.cs
+++ Assets/ObjectGenerate.cs
@@ -9,7 +9,6 @@ public class ObjectGenerate : MonoBehaviour
     private float positionY;
     private float positionZ;
     
-
         private void TargetGenerate() //Targetの生成
     {
         positionX = Random.Range(-8.0f, 8.0f);
@@ -18,7 +17,6 @@ public class ObjectGenerate : MonoBehaviour
         Vector3 targetPosition = new Vector3(positionX,positionY,positionZ);
         GameObject randomTarget = Instantiate(target,targetPosition,Quaternion.identity);
     }
-    
     
     // Start is called before the first frame update
     void Start()
